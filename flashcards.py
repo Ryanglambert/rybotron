e355bcb366717aaf441fdbd02770fe5bdafd767f
Ryan Lambert <rl@saucelabs.com>
2013-12-19 23:22:04 -0800

    blah blah blah

--- flashcards.py
+++ flashcards.py
@@ -1,17 +1,46 @@
-#prompt user with 
-#with open("flashfilename", "a") as x:
-	#x.write("appended text")
+#import os.path
 
+
+
+file_name = raw_input('please select a new filename or an existing one:')
+
+flashcard_file = open(file_name,'w')
+
+#if path.isfile(file_name)  ==  False:
 
 flashcard = {}
 ivd = {v: k for k, v in flashcard.items()}
+x = 0
+
+
+while x != 'stop':
+	print 'type \'stop\' to stop adding words to the dictionary'
+
+	x = raw_input('keyword:')
+
+	if x == 'stop':
+		break
+	else:
+
+		y = raw_input('definition:')
+
+
+	#update dictionary		
+	flashcard [x] = y
+	ivd = {v: k for k, v in flashcard.items()}
+
+	#remove 'stop' from the dict
+	flashcard.pop('stop',0)
+	ivd.pop('stop',0)
+
+
+
+	print flashcard
+	print ivd
+
+flashcard_file.write(flashcard)
+flashcard_file.close()
+
+
 print flashcard
 print ivd
-
-x = raw_input('>>>')
-y = raw_input('>>>')
-
-flashcard[x] = y
-
-print flashcard
-
