e0077e7a907ee547c698e8811a8b41bb5b90f072
Ryan_Lambert <ryanl@rybo.local>
2013-12-29 14:21:53 -0800

    removed check if file exists
    need to understand a better way to detect if a file already exist

--- add_flashcards.py
+++ add_flashcards.py
@@ -2,36 +2,34 @@
 
 file_name = raw_input('please select a new filename or an existing one:')
 
-check_flash_file = open(file_name,'a')
-print 'initial open and write'
-check_flash_file.close()
-print 'initial close'
-check_flash_file = open(file_name)
-print 'open for READ'
-
-if  '{' in check_flash_file.read():
-	flashcard = dict(check_flash_file.read())
-	print "The file exists!"
+flash_file = open(file_name,'r')
+print " opening %r for read " % file_name
+#Check if file contains a dictionary
+if  '{' in flash_file.read():
+    flashcard = dict(flash_file.read())
+    print "The file exists!"
+    print "This is the content of the file"
+    print flash_file.read()
 else:
-	flashcard = {}
-	print "The file doesn't exist!"
-
-check_flash_file.close()
+    flashcard = {}
+    print "The file doesn't exist!"
+    print "flashcard is equal to %r" % flashcard
+flash_file.close()
 
 ivd = {v: k for k, v in flashcard.items()}
 x = 0
 
 
 while x != 'stop':
-	print 'type \'stop\' to stop adding words to the dictionary'
+    print 'type \'stop\' to stop adding words to the dictionary'
 
-	x = raw_input('keyword:')
+    x = raw_input('keyword:')
 
-	if x == 'stop':
+    if x == 'stop':
 		break
-	else:
+    else:
 
-		y = raw_input('definition:')
+        y = raw_input('definition:')
 
 
 	#update dictionary		
